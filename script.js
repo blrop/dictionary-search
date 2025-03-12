ded7b9d5dcd2c4ef2b7692d63506509461796664
Igor Siluianov <olvin.hh@gmail.com>
2025-03-13 00:38:59 +0200

    add second search type and switch; fix time indicator

--- script.js
+++ script.js
@@ -4,10 +4,65 @@ const getDict = async () => {
 	return data.split('\n');
 };
 
+const findViaSimpleIteration = (dict, input) => {
+	let foundItem = null;
+	let i = 0;
+
+	for (let item of dict) {
+		i++;
+		const word = item.toLowerCase();
+
+		if (word.startsWith(input)) {
+			foundItem = item;
+			break;
+		}
+	}
+
+	return {
+		foundItem,
+		iterationsCount: i,
+	};
+};
+
+const findViaBinarySearch = (dict, input) => {
+	let iterationsCount = 0;
+
+	const findBetween = (start, end) => {
+		iterationsCount++;
+
+		const middleIndex = start + Math.floor((end - start) / 2);
+		if (middleIndex === start) {
+			if (dict[start].toLowerCase().startsWith(input)) {
+				return dict[start];
+			} else if (dict[end].toLowerCase().startsWith(input)) {
+				return dict[end];
+			} else {
+				return null;
+			}
+		}
+
+		const middleWord = dict[middleIndex].toLowerCase();
+		if (input <= middleWord) {
+			return findBetween(start, middleIndex);
+		} else {
+			return findBetween(middleIndex + 1, end);
+		}
+	};
+
+	return {
+		foundItem: findBetween(0, dict.length - 1),
+		iterationsCount,
+	};
+};
+
 document.addEventListener('DOMContentLoaded', async () => {
-	const $input = document.getElementById('input');
+	const $input = document.getElementById('text-input');
+	const $searchTypeIteration = document.getElementById('search-type-iteration');
+	const $searchTypeBinary = document.getElementById('search-type-binary');
 	const $status = document.getElementById('status');
 	const $result = document.getElementById('result');
+
+	let useBinarySearch = $searchTypeBinary.checked;
 
 	$status.innerText = 'Loading...';
 
@@ -27,27 +82,31 @@ document.addEventListener('DOMContentLoaded', async () => {
 			return;
 		}
 
-		let foundItem = null;
-		let i = 0;
-		const startTime = new Date();
+		const startTime = performance.now();
+		let result;
 
-		for (let item of dict) {
-			i++;
-			const word = item.toLowerCase();
-
-			if (word.startsWith(input)) {
-				foundItem = item;
-				break;
-			}
+		if (useBinarySearch) {
+			result = findViaBinarySearch(dict, input);
+		} else {
+			result = findViaSimpleIteration(dict, input);
 		}
+		const foundItem = result.foundItem;
+		const iterationsCount = result.iterationsCount;
 
 		if (foundItem) {
-			const time = (new Date()).getTime() - startTime.getTime();
-			$status.innerText = `Found (iterations made: ${i}; time spent: ${time} ms)`;
+			const time = (performance.now() - startTime).toFixed(1);
+			$status.innerText = `Found (iterations made: ${iterationsCount}; time spent: ${time} ms)`;
 			$result.innerText = foundItem;
 		} else {
 			$status.innerText = 'Not found';
 			$result.innerText = '';
 		}
 	});
+
+	const onSearchTypeChange = () => {
+		useBinarySearch = $searchTypeBinary.checked;
+	};
+
+	$searchTypeIteration.addEventListener('change', onSearchTypeChange);
+	$searchTypeBinary.addEventListener('change', onSearchTypeChange);
 });
